9f699801e290a85bdab1be095e93097978196e3d
Nikita Moussa <71714688+Vampir20@users.noreply.github.com>
2021-11-03 16:51:58 +0100

    Add files via upload

---
+++ java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+import com.example.myapplication.ui.pictureOfTheDayFragment
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null ){
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance())
+                .commit()
+        }
+    }
+
+
+}
